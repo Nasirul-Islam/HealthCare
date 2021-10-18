4b3c35b4882bf26d174b7bac3a418854f8d2b52b
Nasirul-Islam <nasirulislam866@gmail.com>
2021-10-18 11:08:57 +0600

    nav and footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import './Footer.css'
+import { Col, Container, Row } from 'react-bootstrap';
+import { FaHome, FaFacebookF, FaGooglePlusG, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import { BsFillEnvelopeFill, BsFillTelephoneFill } from "react-icons/bs";
 
 const Footer = () => {
     return (
-        <div>
-            <small>Copyright © 2021 HealthCare.com</small>
+        <div className="footer">
+            <Container className="">
+                <Row className="py-5">
+                    <Col md={6} className=" text-start">
+                        <h2>HealthCare</h2>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam impedit, quasi rerum sunt aspernatur quas mollitia ea ut sapiente optio laudantium, harum quidem ipsam amet non at explicabo dolorem quod.</p>
+                    </Col>
+                    <Col md={3} className="socialMedia">
+                        <h2>Social Media</h2>
+                        <div className="d-flex justify-content-center social-icon">
+                            <div className="icon">
+                                <p><FaFacebookF /></p>
+                                <p><FaGooglePlusG /></p>
+                                <p><FaTwitter /></p>
+                                <p><FaLinkedinIn /></p>
+                            </div>
+                            <div className="icon-text">
+                                <p>Facebook</p>
+                                <p>Google+</p>
+                                <p>Twitter</p>
+                                <p>Linkedin</p>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col md={3} className="">
+                        <h2>Find Us</h2>
+                        <div className="d-flex justify-content-evenly social-icon">
+                            <div className="icon">
+                                <p><FaHome /></p>
+                                <br />
+                                <br />
+                                <p><BsFillEnvelopeFill /></p>
+                                <p><BsFillTelephoneFill /></p>
+                            </div>
+                            <div className="icon-text">
+                                <p>143 Gordon Terrace Embarrassing NG33 0ZT
+                                    United Kingdom</p>
+                                <p>info@healthcare.com</p>
+                                <p>++0999888</p>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+                <Row className="py-4">
+                    <p className="footer-bootom">Copyright © 2021 HealthCare.com</p>
+                </Row>
+            </Container>
         </div>
     );
 };
--- src/Pages/Shared/Navigation/Navigation.js
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -1,33 +1,51 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import './Navigation.css'
+import './Navigation.css';
+import { FaClock } from "react-icons/fa";
+import { BsFillTelephoneOutboundFill } from "react-icons/bs";
+
 
 const Navigation = () => {
     return (
         <>
-            <Navbar bg="dark" variant="dark">
+            <div className="nav-top">
+                <Container>
+                    <Row>
+                        <Col lg={6} className="d-flex justify-content-start">
+                            <p> <span><FaClock /></span> Opening Hours : 8.00 AM - 5.00 PM ( Sat - Thu )</p>
+                        </Col>
+                        <Col lg={6} className="d-flex justify-content-end">
+                            <p> <span><BsFillTelephoneOutboundFill /></span> Phone : +(0321) 752 8659</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+            <Navbar collapseOnSelect expand="sm" bg="light" variant="light">
                 <Container className="navigation">
                     <Navbar.Brand href="#home">
-                        HealthCare
+                        <h1>HealthCare</h1>
                     </Navbar.Brand>
-                    <Nav className="ms-auto">
-                        <Nav.Link href="#home">
-                            <Link to="/home">Home</Link>
-                        </Nav.Link>
-                        <Nav.Link href="#features">
-                            <Link to="/about">About</Link>
-                        </Nav.Link>
-                        <Nav.Link href="#pricing">
-                            <Link to="/blog">Blog</Link>
-                        </Nav.Link>
-                        <Nav.Link href="#pricing">
-                            <Link to="/blog">Log In</Link>
-                        </Nav.Link>
-                        <Nav.Link href="#pricing">
-                            <Link to="/blog">Log Out</Link>
-                        </Nav.Link>
-                    </Nav>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="#home">
+                                <Link to="/home">Home</Link>
+                            </Nav.Link>
+                            <Nav.Link href="#features">
+                                <Link to="/about">About</Link>
+                            </Nav.Link>
+                            <Nav.Link href="#pricing">
+                                <Link to="/blog">Blog</Link>
+                            </Nav.Link>
+                            <Nav.Link href="#pricing">
+                                <Link to="/blog">Log In</Link>
+                            </Nav.Link>
+                            <Nav.Link href="#pricing">
+                                <Link to="/blog">Log Out</Link>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </>
